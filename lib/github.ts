26d637ce935debd9f94f05857d318e57fabc23e1
TheoDerive <theo.derive.pro@gmail.com>
2024-12-16 14:29:48 +0100

    first commit!

--- lib/github.ts
+++ lib/github.ts
@@ -2,14 +2,17 @@ import * as Git from "simple-git";
 
 export async function Test() {
   try {
-    Git.simpleGit({
+    await Git.simpleGit({
       baseDir: "/home/thyo/Marine-Portfolio/",
       binary: "git",
     })
       .init()
       .add("./*")
       .commit("first commit!")
-      .addRemote("website", "git@github.com:TheoDerive/Marine-Portfolio.git")
+      .addRemote(
+        "website",
+        "https://github.com/TheoDerive/Marine-Portfolio.git",
+      )
       .exec(() => console.log("pass"))
       .push("website", "master", [], () => console.log("done"));
   } catch (error) {

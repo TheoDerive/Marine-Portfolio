9fc5087f7efdadcd4ff03c66613e15ef5d524514
TheoDerive <theo.derive.pro@gmail.com>
2024-12-16 11:10:12 +0100

    first commit!

--- lib/github.ts
+++ lib/github.ts
@@ -4,8 +4,8 @@ export async function Test() {
   try {
     Git.simpleGit()
       .init()
-      .exec(() => console.log("padd"))
       .add("./*")
+      .exec(() => console.log("padd"))
       .commit("first commit!")
       .addRemote("origin", "git@github.com:TheoDerive/Marine-Portfolio.git")
       .push("origin", "master", [], () => console.log("done"));

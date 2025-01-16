92ed45c03fe87bb00bcc7ac5ca4be1336c5fde4f
TheoDerive <theo.derive.pro@gmail.com>
2025-01-16 15:40:37 +0100

    Remove global declaration

--- types/global.d.ts
+++
@@ -1,9 +0,0 @@
-// global.d.ts
-import mongoose from "mongoose";
-
-declare global {
-  let mongoose: {
-    conn: mongoose.Connection | null;
-    promise: Promise<mongoose.Connection> | null;
-  };
-}
